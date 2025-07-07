d11beb00e5c2dbecc8442be4cc19a7856f49db1b
rajavenkatesh04 <grv.9604@gmail.com>
2025-07-07 08:10:09 +0530

    Added Clarity analytics

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
-import { GeistSans, GeistMono } from 'geist/font';
+import Script from 'next/script'
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import Navbar from "@/_components/ui/Navbar";
-import Footer from "@/_components/ui/Footer";
 import {Providers} from "@/Providers";
 
 export const metadata = {
@@ -14,6 +13,20 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
         <head>
+
+            <Script
+                id="microsoft-clarity-analytics"
+                strategy="afterInteractive"
+            >
+                {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sayk3j4wyp");
+          `}
+            </Script>
+
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link
                 rel="stylesheet"
