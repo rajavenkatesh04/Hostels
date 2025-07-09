d147cbc6f3510691f038601330937920b6c5716c
rajavenkatesh04 <grv.9604@gmail.com>
2025-07-09 11:37:55 +0530

    Updated router push target in /choose

--- app/hostel/HostelDetailsContent.js
+++ app/hostel/HostelDetailsContent.js
@@ -60,8 +60,6 @@ export default function HostelDetailsContent() {
         router.push(`/ProceedToBooking`);
     }
 
-
-
     if (isLoading) {
         return (
             <div className="min-h-screen flex flex-col items-center justify-center p-6 bg-white">
